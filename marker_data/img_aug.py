c6c4686439d4a4e7ea4ee4e4daac7bc78282656e
Yang Ding <dyt811@gmail.com>
2018-09-15 13:21:57 -0400

    Added code to generate new images and other essential functions.

--- marker_data/img_aug.py
+++ marker_data/img_aug.py
@@ -43,12 +43,12 @@ def ImageAugmentator(image_path, out_path, aug_seq, iterations):
         # Will have a removal failure.
 
 
-def FolderAugmentator(folder_path, out_path, aug_seg, iterations):
+def FolderAugmentator(input_folder_path, out_path, aug_seg, iterations):
     # TODO: currently it seems to have some trouble reading from a folder and then CLOSE the god damn thing.
 
     """
     Augment the entire folder, using the augmentation sequence provided over the number of times requests.
-    :param folder_path:
+    :param input_folder_path:
     :param out_path:
     :param aug_seg:
     :param iterations:
@@ -56,25 +56,25 @@ def FolderAugmentator(folder_path, out_path, aug_seg, iterations):
     """
 
     # Generate temporary directory.
-    with TemporaryDirectory() as temporary_path:
-        input_files = os.listdir(folder_path)
+    #with TemporaryDirectory() as temporary_path:
+    input_files = os.listdir(input_folder_path)
 
-        # Duplicate the folder x times
-        for x in range(0, iterations):
-            os.chdir(folder_path)
-            # each time, duplicate all the files within it
-            for file in input_files:
-                new_file_name = os.path.join(temporary_path, unique_name() + ".png")
-                shutil.copyfile(file, new_file_name)
+    # Duplicate the folder x times
+    for x in range(0, iterations):
+        os.chdir(input_folder_path)
+        # each time, duplicate all the files within it
+        for file in input_files:
+            new_file_name = os.path.join(out_path, unique_name() + ".png")
+            shutil.copyfile(file, new_file_name)
 
-        # Load images into a giant matrices from the TEMP folder
-        images_ndarray = from_folder(temporary_path)
+    # Load images into a giant matrices from the TEMP folder
+    images_ndarray = from_folder(out_path)
 
-        # Augment the giant matrices
-        images_augmented = aug_seg.augment_images(images_ndarray)
+    # Augment the giant matrices
+    images_augmented = aug_seg.augment_images(images_ndarray)
 
-        # Save the augmented images out to path.
-        save_images(images_augmented, out_path)
+    # Save the augmented images out to path.
+    save_images(images_augmented, out_path)
 
 
 if __name__ == "__main__":
