4dd2385978d87c2dacbcc5bac5d59be6261c300d
Yang Ding <dyt811@gmail.com>
2018-10-25 12:46:51 +0800

    Added external .env loading for major path

--- CNN3_1080.py
+++ CNN3_1080.py
@@ -1,6 +1,5 @@
 from keras.models import Sequential
 from keras.layers import Dense, Conv2D, MaxPooling2D, Dropout, Flatten, Activation
-from keras.preprocessing.image import ImageDataGenerator
 from keras.callbacks import TensorBoard, ModelCheckpoint
 from keras.layers import LeakyReLU
 import keras
@@ -10,6 +9,7 @@ from PythonUtils.file import unique_name, filelist_delete, recursive_list
 from generator.PoseDataSequence import DataSequence
 from generator.csvgen import generate_csv
 from PythonUtils.folder import get_abspath
+from dotenv import load_dotenv
 
 def cleanLog(input_path):
     if input_path is None:
@@ -25,22 +25,21 @@ def cleanLog(input_path):
             filelist_delete(files)
 
 def load_data_and_run(model,input_shape, TBCallBack):
-    train_path          = r"C:\Yang\Dropbox\Machine_Learning\orientation_training"
-    train_csv_path      = r"C:\Yang\Dropbox\Machine_Learning\train.csv"
-    validate_path       = r"C:\Yang\Dropbox\Machine_Learning\orientation_training"
-    validate_csv_path   = r"C:\Yang\Dropbox\Machine_Learning\validate.csv"
+    load_dotenv()
+    train_path          = os.getenv("train_path")
+    train_csv_path      = os.getenv("train_csv_path")
+    validate_path       = os.getenv("validate_path")
+    validate_csv_path   = os.getenv("validate_csv_path")
 
     # Dynamicly generate model path.
     project_root = os.path.realpath(__file__)
-    model_path = os.path.join(os.path.dirname(project_root),"models")
+    model_path = os.path.join(os.path.dirname(project_root), "models")
 
     generate_csv(train_path, train_csv_path )
     train_data = DataSequence(train_csv_path, 100, mode="Train")
 
     generate_csv(validate_path,validate_csv_path)
     validation_data = DataSequence(validate_csv_path, 100)
-
-    get_abspath()
 
     model_name = os.path.join(model_path, unique_name())
 
@@ -71,29 +70,29 @@ def createModel(input_shape, output_classes):
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.2))
 
-    model.add(Conv2D(32, (3, 3), padding='same', strides=(1,1)))
-    model.add(LeakyReLU(alpha=0.1))
-    #model.add(Conv2D(64, (5, 5), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.2))
-
-    model.add(Conv2D(32, (3, 3), padding='same', strides=(1,1)))
-    model.add(LeakyReLU(alpha=0.1))
-    #model.add(Conv2D(64, (5, 5), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.2))
-
-    model.add(Conv2D(64, (3, 3), padding='same', strides=(1,1)))
-    model.add(LeakyReLU(alpha=0.1))
-    #model.add(Conv2D(64, (5, 5), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.2))
-
-    model.add(Conv2D(64, (3, 3), padding='same', strides=(1, 1)))
-    model.add(LeakyReLU(alpha=0.1))
-    # model.add(Conv2D(64, (5, 5), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.2))
+    # model.add(Conv2D(32, (3, 3), padding='same', strides=(1,1)))
+    # model.add(LeakyReLU(alpha=0.1))
+    # #model.add(Conv2D(64, (5, 5), activation='relu'))
+    # model.add(MaxPooling2D(pool_size=(2, 2)))
+    # model.add(Dropout(0.2))
+    #
+    # model.add(Conv2D(32, (3, 3), padding='same', strides=(1,1)))
+    # model.add(LeakyReLU(alpha=0.1))
+    # #model.add(Conv2D(64, (5, 5), activation='relu'))
+    # model.add(MaxPooling2D(pool_size=(2, 2)))
+    # model.add(Dropout(0.2))
+    #
+    # model.add(Conv2D(64, (3, 3), padding='same', strides=(1,1)))
+    # model.add(LeakyReLU(alpha=0.1))
+    # #model.add(Conv2D(64, (5, 5), activation='relu'))
+    # model.add(MaxPooling2D(pool_size=(2, 2)))
+    # model.add(Dropout(0.2))
+    #
+    # model.add(Conv2D(64, (3, 3), padding='same', strides=(1, 1)))
+    # model.add(LeakyReLU(alpha=0.1))
+    # # model.add(Conv2D(64, (5, 5), activation='relu'))
+    # model.add(MaxPooling2D(pool_size=(2, 2)))
+    # model.add(Dropout(0.2))
 
     model.add(Flatten())
     model.add(Dense(1024))
@@ -108,13 +107,17 @@ def createModel(input_shape, output_classes):
 if __name__ =="__main__":
     from time import time
 
-    cleanLog()
+    cleanLog(None)
     image_size = 480
-    model1 = createModel(image_size, 4) # downsize to 128
+    model1 = createModel(image_size, 3) # downsize to 128
     model1.compile(loss="sparse_categorical_crossentropy", optimizer="adadelta", metrics=["acc", "mae"])
 
+    # Dynamicly generate model input_path.
+    project_root = os.path.realpath(__file__)
+    log_path = os.path.join(os.path.dirname(project_root), "logs")
+
     tensorboard = keras.callbacks.TensorBoard(
-        log_dir=r'/home/dyt811/Git/MarkerTrainer/logs',
+        log_dir=log_path,
         histogram_freq=0,
         write_images=True)
 
---
+++ dotenvtemplate.md
@@ -0,0 +1,4 @@
+    train_path          = r"C:\Yang\Dropbox\Machine_Learning\orientation_training"
+    train_csv_path      = r"C:\Yang\Dropbox\Machine_Learning\train.csv"
+    validate_path       = r"C:\Yang\Dropbox\Machine_Learning\orientation_training"
+    validate_csv_path   = r"C:\Yang\Dropbox\Machine_Learning\validate.csv"
