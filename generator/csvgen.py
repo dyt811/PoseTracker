d35644f9bd21a9d1c60aeaf9effb7feaac2edd30
Yang Ding <dyt811@gmail.com>
2018-10-27 09:13:46 +0800

    Added the ability to clean up folder for machine learning.

--- generator/csvgen.py
+++ generator/csvgen.py
@@ -48,6 +48,35 @@ def generate_csv(folder_path, csv_path):
 
     return csv_path
 
+def delete_not_recognized_images(folder_path):
+    """
+    WARNING: DESTRUCTIVE FUNCTION. Use with care.
+    Given a path, it will check if images has a marker recognized. If not, it will delete the images. This is mainly used
+    to clean up the raw data input stream so that only images with paired JSON files will be sent off for machine learning trainings.
+
+    :param folder_path: the path to the folder to carry out this operation.
+    :return:
+    """
+    from PythonUtils.folder import recursive_list
+    file_list = recursive_list(folder_path)
+    accepted_extensions=('png', 'jpg', 'gif', 'jpeg', 'tif', 'bmp')
+
+
+    for file in file_list:
+        # Header image header name.
+        image_format = imghdr.what(file)
+
+        # Check if they are anticipated.
+        if image_format not in accepted_extensions:
+            continue # skip file if image format is not compatible.
+
+        # Check if JSON file exist.
+        anticipated_JSON = file + ".ROI.json"
+        if not os.path.exists(anticipated_JSON):
+            os.remove(file)
+
+
+
 def generate_train_sequence(csv_path):
     training_sequence = DataSequence(csv_path, 100, mode="Train")
     return training_sequence
